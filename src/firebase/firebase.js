7175e6e18084de7b18849db57e9ac1a762de4568
Ricky Raghavan <rajivsr@yahoo.com>
2019-02-22 13:58:28 -0500

    Connected the application to Firebase. Tested it by Writing a name property

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,22 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyCD9KIos-ngmciCahdusagisJaiV8EO4D4",
+  authDomain: "expensify-84681.firebaseapp.com",
+  databaseURL: "https://expensify-84681.firebaseio.com",
+  projectId: "expensify-84681",
+  storageBucket: "expensify-84681.appspot.com",
+  messagingSenderId: "673144052017"
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
+
+database.ref().set({
+name:'Ricky Raghavan'
+
+});
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export { firebase, googleAuthProvider, database as default };
